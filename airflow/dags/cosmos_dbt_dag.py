b27b0b6b694829feb3b5fc8920567e174bf30428
Dorian <dorianteffo@gmail.com>
2024-03-21 16:08:02 +0100

    dbt dag

--- airflow/dags/cosmos_dbt_dag.py
+++ airflow/dags/cosmos_dbt_dag.py
@@ -1,4 +1,4 @@
-from cosmos import DbtTaskGroup, ProjectConfig, ProfileConfig, ExecutionConfig
+from cosmos import DbtTaskGroup, ProjectConfig, ProfileConfig, ExecutionConfig, DbtDag
 from cosmos.profiles import SnowflakeUserPasswordProfileMapping
 from datetime import datetime 
 from airflow.decorators import dag
@@ -27,35 +27,50 @@ profile_config_prod = ProfileConfig(
     ),
 )
 
-@dag(
+# @dag(
+#     schedule_interval="@daily",
+#     start_date=datetime(2024, 3, 20),
+#     catchup=False,
+#     tags=["dbt", "snowflake"],
+#     dag_id='dbt-snowflake-dag'
+# )
+# def dbt_dag():
+#     transform_dev = DbtTaskGroup(
+#         group_id = "dbt_dev",
+#         project_config=ProjectConfig(DBT_PROJECT_PATH),
+#         execution_config = ExecutionConfig(dbt_executable_path = DBT_EXECUTABLE_PATH),
+#         profile_config=profile_config_dev,
+#         default_args={"retries": 2}
+#     )
+
+
+#     transform_prod = DbtTaskGroup(
+#         group_id = "dbt_prod",
+#         project_config=ProjectConfig(DBT_PROJECT_PATH),
+#         execution_config = ExecutionConfig(dbt_executable_path = DBT_EXECUTABLE_PATH),
+#         profile_config=profile_config_prod,
+#         default_args={"retries": 2}
+#     )
+
+
+#     transform_dev >> transform_prod
+
+# dbt_dag()
+
+
+dbt_dag = DbtDag(
+    project_config=ProjectConfig(DBT_PROJECT_PATH),
+    execution_config = ExecutionConfig(dbt_executable_path = DBT_EXECUTABLE_PATH),
+    profile_config=profile_config_prod,
+    operator_args={
+        "install_deps":True
+    },
+    default_args={"retries": 2},
     schedule_interval="@daily",
     start_date=datetime(2024, 3, 20),
     catchup=False,
     tags=["dbt", "snowflake"],
     dag_id='dbt-snowflake-dag'
 )
-def dbt_dag():
-    transform_dev = DbtTaskGroup(
-        group_id = "dbt_dev",
-        project_config=ProjectConfig(DBT_PROJECT_PATH),
-        execution_config = ExecutionConfig(dbt_executable_path = DBT_EXECUTABLE_PATH),
-        profile_config=profile_config_dev,
-        default_args={"retries": 2}
-    )
-
-
-    transform_prod = DbtTaskGroup(
-        group_id = "dbt_prod",
-        project_config=ProjectConfig(DBT_PROJECT_PATH),
-        execution_config = ExecutionConfig(dbt_executable_path = DBT_EXECUTABLE_PATH),
-        profile_config=profile_config_prod,
-        default_args={"retries": 2}
-    )
-
-
-    transform_dev >> transform_prod
-
-dbt_dag()
-
 
 
