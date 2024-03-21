345764d9e5f49a742aa2c5bbeea4176f9d01b93e
Dorian <dorianteffo@gmail.com>
2024-03-21 15:52:37 +0100

    edit dag schedule

--- airflow/dags/cosmos_dbt_dag.py
+++ airflow/dags/cosmos_dbt_dag.py
@@ -29,7 +29,7 @@ profile_config_prod = ProfileConfig(
 
 @dag(
     schedule_interval="@daily",
-    start_date=datetime(2024, 3, 22),
+    start_date=datetime(2024, 3, 20),
     catchup=False,
     tags=["dbt", "snowflake"],
     dag_id='dbt-snowflake-dag'
